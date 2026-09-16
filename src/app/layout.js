668636dda07db8bd7499dc8ffb9da70673841f59
sharmakush2003 <sharmakush2003@users.noreply.github.com>
2026-09-16 11:29:10 +0530

    feat(meta): add facebook domain verification tag for chittortech.in

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,6 +75,7 @@ export default function RootLayout({ children }) {
         <meta name="google-site-verification" content="CY_ChPltJ7jb_m5ZzUqCb63jBILSU2uOv7bYNgJc3y8" />
         <meta name="yandex-verification" content="495c64862381b7b9" />
         <meta name="p:domain_verify" content="83a539ac9f26d5e2f0b714eee5cca03f"/>
+        <meta name="facebook-domain-verification" content="opaaiyfyje7nkw6e2xqtbk7cz4llul" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
